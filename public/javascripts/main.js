be78ead6451b24b9ce672ff4556b724ae211ae18
Gilbert Chamaa <gilbert.chamaa@wellsfargo.com>
2016-10-13 10:09:38 -0700

    Passing the clickHandler into the component

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,7 +1,7 @@
 var GeneralPurposeButton = React.createClass({
   render: function(){
-    return (<button type={this.props.type} id={this.props.id} className={this.props.className}>{this.props.label}</button>)
+    return (<button type={this.props.type} id={this.props.id} className={this.props.className} onClick={this.props.onClick}>{this.props.label}</button>)
   }
 });
 
-ReactDOM.render(<GeneralPurposeButton label="Add Task" type="button" id="addTask" className="genButton"/>, document.querySelector('#main'));
+ReactDOM.render(<GeneralPurposeButton label="Add Task" type="button" id="addTask" className="genButton" onClick={function(){alert('hi')}}/>, document.querySelector('#main'));
