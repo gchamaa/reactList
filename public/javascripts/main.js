e1342847a4e6e647ed22845a0f607226d46dadf3
Gilbert Chamaa <gchamaa@yahoo.com>
2016-10-18 01:02:46 -0700

    Adding the AddTaskModal

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,7 +1,20 @@
-var GeneralPurposeButton = React.createClass({
+// TO DO
+// Use ES6 class, use ES6 function syntax
+var AddTaskModal = React.createClass({
   render: function(){
-    return (<button type={this.props.type} id={this.props.id} className={this.props.className} onClick={this.props.onClick}>{this.props.label}</button>)
+    return (<div id={this.props.id} className={this.props.className}>
+              <label htmlFor="addTaskControl">Add Task</label>
+              <input type="text" id="addTaskControl"/>
+              <div>
+                <button type="button" id="closeModal" className="closeModal" name="closeModal">Close</button>
+                <button type="button" id="saveModal" className="saveModal" name="saveModal">Save</button>
+              </div>
+            </div>)
   }
 });
 
-ReactDOM.render(<GeneralPurposeButton label="Add Task" type="button" id="addTask" className="genButton" onClick={function(){alert('hi')}}/>, document.querySelector('#main'));
+function openModal (){
+  ReactDOM.render(<AddTaskModal id="addTaskModal" className="addTaskModal"></AddTaskModal>, document.querySelector('#modal'))
+}
+
+document.querySelector('#addTaskButton').addEventListener('click', openModal);
